2b968e9b56c97280b77cfe804da787e764e7b918
Bruno Abrantes <inf0rmer.realm@gmail.com>
2014-06-13 16:10:31 +0100

    Separates utilities into their own module

--- src/everlist.js
+++ src/everlist.js
@@ -1,52 +1,4 @@
-// Utilities
-var now = Date.now || function() { return new Date().getTime(); };
-
-var debounce = function(func, wait, immediate) {
-  var timeout, args, context, timestamp, result;
-
-  var later = function() {
-    var last = now() - timestamp;
-    if (last < wait) {
-      timeout = setTimeout(later, wait - last);
-    } else {
-      timeout = null;
-      if (!immediate) {
-        result = func.apply(context, args);
-        context = args = null;
-      }
-    }
-  };
-};
-
-var ctor = function(){};
-
-var bind = function(func, context) {
-    var args, bound, nativeBind = Function.prototype.bind, slice = Array.prototype.slice;
-
-    if (nativeBind && func.bind === nativeBind) {
-      return nativeBind.apply(func, slice.call(arguments, 1));
-    }
-
-    if (typeof func !== 'function') {
-      throw new TypeError();
-    }
-
-    args = slice.call(arguments, 2);
-    return bound = function() {
-      if (!(this instanceof bound)) {
-        return func.apply(context, args.concat(slice.call(arguments)));
-      }
-
-      ctor.prototype = func.prototype;
-      var self = new ctor();
-      ctor.prototype = null;
-      var result = func.apply(self, args.concat(slice.call(arguments)));
-      if (Object(result) === result) {
-        return result;
-      }
-      return self;
-    };
-  };
+var utilities = require('./utilities');
 
 // Main object
 var Everlist = (function() {
@@ -70,8 +22,8 @@ var Everlist = (function() {
   }
 
   Everlist.prototype.startMonitoring = function() {
-    var debouncedMonitor = debounce(this.monitor, this.options.timeout);
-    this.$el.on('scroll.everlist', bind(this.monitor, this));
+    var debouncedMonitor = utilities.debounce(this.monitor, this.options.timeout);
+    this.$el.on('scroll.everlist', utilities.bind(this.monitor, this));
   };
 
   Everlist.prototype.monitor = function() {
